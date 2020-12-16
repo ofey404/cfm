54d4abd735fb58060426be69341aef80f84b2eb3
Ofey Chan <ofey206@gmail.com>
2020-12-17 00:07:05 +0800

    Get execute location from gdb output. With a custom package of gdb.

--- src/debug.rs
+++ src/debug.rs
@@ -1,22 +1,38 @@
 use gdb;
 use gdb::StreamRecord;
+use regex::Regex;
+use lazy_static::lazy_static;
+use gdb::StreamRecord::Console;
+use crate::debug::Error::ParseError;
+use gdb::Record::Stream;
+
+lazy_static! {
+    static ref execute_location_re: Regex = Regex::new(r"^.+?(0x[a-z0-9]{16}) in (.+?) .+$").unwrap();
+}
 
 #[derive(Debug)]
 struct ExecuteLocation {
-    address: u64,
+    address: String,
     function: String,
 }
 
-// #[derive(Debug)]
-// pub enum Error {
-// }
-//
-// impl ExecuteLocation {
-//     fn from_stream_record(s: StreamRecord) -> Result<ExecuteLocation, Error>{
-//
-//     }
-// }
-//
+#[derive(Debug)]
+pub enum Error {
+    ParseError,
+}
+
+impl ExecuteLocation {
+    fn from_stream_record(s: &StreamRecord) -> Result<ExecuteLocation, Error>{
+        let info_str = match s {
+            Console(info_str) => info_str,
+            _ => return Err(ParseError),
+        };
+
+        let caps = execute_location_re.captures(info_str.as_str()).unwrap();
+        Ok(ExecuteLocation{ address: (&caps[1]).to_string(), function: (&caps[2]).to_string() })
+    }
+}
+
 fn get_core_names(path: String) -> String {
     return String::from("./tests/hello/core.26772");
 }
@@ -29,5 +45,12 @@ pub fn run_gdb() {
     let response = debugger.send_cmd_raw("core ./tests/hello/core.26772\n").unwrap();
     println!("{:?}", response);
     let response = debugger.send_cmd_raw_full_record("bt\n").unwrap();
-    println!("{:?}", response);
+    println!("{:?}", &response);
+
+    let location = match &response[1] {
+        Stream(s) => ExecuteLocation::from_stream_record(s).unwrap(),
+        _ => return
+    };
+    println!("location: {:?}", location);
+    println!("Over");
 }
