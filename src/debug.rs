c4c171e6746261a116ab7c385edada1208f18ef6
Ofey Chan <ofey206@gmail.com>
2020-12-18 11:47:48 +0800

    Refactor main logic, remove previous draft.

--- src/debug.rs
+++ src/debug.rs
@@ -12,7 +12,7 @@ lazy_static! {
 }
 
 #[derive(Debug)]
-struct ExecuteLocation {
+struct ExecLocation {
     address: String,
     function: String,
 }
@@ -22,41 +22,83 @@ pub enum Error {
     ParseError,
 }
 
-impl ExecuteLocation {
-    fn from_stream_record(s: &StreamRecord) -> Result<ExecuteLocation, Error> {
-        let info_str = match s {
-            Console(info_str) => info_str,
-            _ => return Err(ParseError),
-        };
-
-        let caps = execute_location_re.captures(info_str.as_str()).unwrap();
-        Ok(ExecuteLocation {
+impl ExecLocation {
+    fn from_stream_record(s: &String) -> Result<ExecLocation, Error> {
+        let caps = execute_location_re.captures(s.as_str()).unwrap();
+        Ok(ExecLocation {
             address: (&caps[1]).to_string(),
             function: (&caps[2]).to_string(),
         })
     }
 }
 
+fn exec_path_to_string(exec_locations: &Vec<ExecLocation>) -> String {
+    let mut ans = "".to_string();
+    for location in exec_locations {
+        let s = &format!("{} {}\n", location.address, location.function);
+        ans.push_str(s);
+    }
+    ans
+}
+
 fn get_core_names(path: String) -> String {
     return String::from("./tests/hello/core.26772");
 }
 
-pub fn run_gdb() {
-    // Run GDB with command
-    let mut debugger = gdb::Debugger::start().unwrap();
-    let response = debugger.send_cmd_raw("file ./tests/hello/flaw\n").unwrap();
-    println!("{:?}", response);
-    let response = debugger
-        .send_cmd_raw("core ./tests/hello/core.26772\n")
-        .unwrap();
-    println!("{:?}", response);
-    let response = debugger.send_cmd_raw_full_record("bt\n").unwrap();
-    println!("{:?}", &response);
+pub fn get_first_core_name() -> String {
+    return String::from("./tests/hello/core.26772");
+}
 
-    let location = match &response[1] {
-        Stream(s) => ExecuteLocation::from_stream_record(s).unwrap(),
-        _ => return,
-    };
-    println!("location: {:?}", location);
-    println!("Over");
+pub fn get_exec_path(flaw_file_path: &str, dump_file_path: &str) -> String {
+    let mut debugger = gdb::Debugger::start().unwrap();
+    let file_cmd = &format!("file {}\n", flaw_file_path);
+    let response = debugger.send_cmd_raw(file_cmd).unwrap();
+    let core_cmd = &format!("core {}\n", dump_file_path);
+    let response = debugger.send_cmd_raw(core_cmd).unwrap();
+    let response = debugger.send_cmd_raw_full_record("bt\n").unwrap();
+
+    let mut exec_locations = Vec::new();
+
+    for record in &response {
+        match &record {
+            Stream(s) => match s {
+                Console(c) => exec_locations.push(ExecLocation::from_stream_record(c).unwrap()),
+                _ => continue,
+            },
+            _ => continue,
+        };
+    }
+    exec_path_to_string(&exec_locations)
+}
+
+pub fn run_gdb() {
+    // // Run GDB with command
+    // let mut debugger = gdb::Debugger::start().unwrap();
+    // let response = debugger.send_cmd_raw("file ./tests/hello/flaw\n").unwrap();
+    // println!("{:?}", response);
+    // let response = debugger
+    //     .send_cmd_raw("core ./tests/hello/core.26772\n")
+    //     .unwrap();
+    // println!("{:?}", response);
+    // let response = debugger.send_cmd_raw_full_record("bt\n").unwrap();
+    // println!("{:?}", &response);
+    //
+    // let mut exec_locations = Vec::new();
+    //
+    // for record in &response {
+    //     let location = match &record {
+    //         Stream(s) => match s {
+    //             Console(c) => exec_locations.push(ExecLocation::from_stream_record(c).unwrap()),
+    //             _ => continue,
+    //         }
+    //         _ => continue,
+    //     };
+    // }
+    // println!("{}", exec_path_to_string(&exec_locations));
+    // println!("Over");
+
+    println!(
+        "{}",
+        get_exec_path("./tests/hello/flaw", "tests/hello/core.26772")
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use ctrlc;
 use std::fs::File;
 use std::io;
 use std::io::{BufWriter, Error, Read, Write};
-use std::process::{Command, Stdio, Output};
+use std::process::{exit, Command, ExitStatus, Output, Stdio};
 use std::thread::sleep;
 use std::{fs, time};
 
@@ -61,11 +61,15 @@ fn run_fuzz(fuzz_file: &str, input: &str) -> Result<Output, Error> {
     child.wait_with_output()
 }
 
+fn exit_with_sigsegv(ecode: ExitStatus) -> bool {
+    !(ecode.success() || !ecode.code().is_none())
+}
+
 fn main() -> Result<(), Error> {
     ctrlc::set_handler(cc_handler).expect("Set ctrl-c handler failed.");
 
     let opts: Opts = Opts::parse();
-    let mut inputs = get_inputs(&opts.input)?;
+     mut inputs = get_inputs(&opts.input)?;
     let mut mutators: Vec<InputMutator> = generate_mutators(&inputs)?;
 
     // Loop all in round robin style.
@@ -73,66 +77,17 @@ fn main() -> Result<(), Error> {
         for mutator in mutators.iter_mut() {
             mutator.mutate();
             let output = run_fuzz(&opts.fuzz_file, mutator.get_mutation())?;
-            // TODO: Analyse.
+            let ecode = output.status;
+            if !exit_with_sigsegv(ecode) {
+                continue;
+            }
+            let mut exec_path = String::new();
+            exec_path = debug::get_exec_path(&opts.fuzz_file, &debug::get_first_core_name());
+            println!("{}", exec_path);
             // TODO: Update output.
         }
         sleep(time::Duration::from_secs(2));
     }
-
-    Ok(())
-}
-
-// TODO: Sketch, Waiting to be removed
-fn __main() -> Result<(), Error> {
-    let opts: Opts = Opts::parse();
-
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    println!("Input: {}", opts.input);
-    println!("Output: {}", opts.output);
-    println!("Fuzzfile: {}", opts.fuzz_file);
-
-    // Read a input file
-    let mut input_file = File::open(&opts.input)?;
-    let mut input = String::new();
-    input_file.read_to_string(&mut input)?;
-
-    println!("File content:{}", &input);
-
-    // Write a output file
-    {
-        let mut output_file = File::create(opts.output)?;
-        output_file.write_all(b"This is output")?;
-    }
-
-    // Fire a subroutine, pass argument to it.
-    let child = Command::new(opts.fuzz_file)
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()?;
-    {
-        let mut child_stdin = (&child).stdin.as_ref().unwrap();
-        let mut writer = BufWriter::new(&mut child_stdin);
-        writer.write_all(input.as_bytes()).unwrap();
-        writer.flush().unwrap();
-    }
-
-    // Collect its output and its return value.
-    let output = child.wait_with_output()?;
-
-    println!("{}", String::from_utf8(output.stdout).unwrap());
-
-    let ecode = output.status;
-    // Check its return value
-    if ecode.success() {
-        println!("flaw run successful");
-    } else {
-        match ecode.code() {
-            Some(code) => println!("flaw failed with code {}", code),
-            None => println!("process terminated by signal"),
-        }
-    }
-
-    debug::run_gdb();
 
     Ok(())
 }
